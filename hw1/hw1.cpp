4a67d30d55c81d0bbdbb8e1d812f11bdcf7a47ff
“welinn” <susutruelove@gmail.com>
2018-03-13 12:37:53 +0800

    down

--- hw1/hw1.cpp
+++ hw1/hw1.cpp
@@ -5,20 +5,16 @@ using namespace std;
 
 int main(int argc, char* argv[]){
 
-  string file[4] = {"./Bunny/", "./Eagle/", "./Teapot/", "./Venus/"};
+  string file[4] = {"Bunny", "Eagle", "Teapot", "Venus"};
   char data[50];
   float para[105];
   int count = 0;
 
   FILE *parmeter = fopen("./Bunny/camera parmeter.txt.txt", "r");
 
+  //read camera parameter file
   while(fgets(data, 50, parmeter) != NULL){
-    /*
-    if((data[0] >= '0' && data[0] <= '9') || data[0] == '-'){
-      printf("%s\n", data);
-      int len = strlen(data);
-      fseek(parmeter, -len, SEEK_CUR);
-    }*/
+
     if(data[0] == '#'){
       if(count < 9){
         for(int i = 0; i < 9; i++){
@@ -37,12 +33,15 @@ int main(int argc, char* argv[]){
   count = 0;
   vector<Mat> Rt;
   Mat K(3, 3, CV_32F);
+
+  //set intrinsic parameter
   for(int i = 0; i < 3; i++){
     for(int j = 0; j < 3; j++){
       K.at<float>(i, j) = para[count++];
     }
   }
 
+  //set extrinsic parameter
   for(int i = 0; i < 8; i++){
     Mat rt(3, 4, CV_32F);
     for(int j = 0; j < 3; j++){
@@ -56,25 +55,24 @@ int main(int argc, char* argv[]){
   ////////////////////////////////////////////////////////////////////
   ////////////////////////////////////////////////////////////////////
 
-
   char filename[20];
   stringstream ss;
 
   for(int i = 0; i < 4; i++){  // 4 folder
     ss.str("");
     ss.clear();
-    ss << file[i] << "output.xyz";
+    ss << "./"<< file[i] << ".xyz";
     ss >> filename;
     FILE *output = fopen(filename, "w");
     vector<Mat> src;
 
-    for(int j = 1; j < 9; j++){ // 8 source image
+    for(int j = 1; j < 9; j++){ //read 8 source image
       ss.str("");
       ss.clear();
-      ss << file[i] << "00" << j << ".bmp";
+      ss << "./" << file[i] << "/00" << j << ".bmp";
       ss >> filename;
       Mat img;
-      img = imread(filename);
+      img = imread(filename, 0);
       src.push_back(img);
     }
 
@@ -87,35 +85,26 @@ int main(int argc, char* argv[]){
     for(int x = -50; x < 50; x++){
       for(int y = -50; y < 50; y++){
         for(int z = -10; z < 90; z++){
-          wPos.at<float>(0, 0) = x;
-          wPos.at<float>(1, 0) = y;
-          wPos.at<float>(2, 0) = z;
-          wPos.at<float>(3, 0) = 1;
-
-          //check the position(world coordinate) in image is black or white
-          //TODO pos wrong
+          wPos = (Mat_<float>(4, 1) << x, y, z, 1);
           background = false;
+          //check the position(world coordinate) in image is black or white
           for(int j = 0; j < 8; j++){
             imgPos = K * Rt[j] * wPos;
+            //normalize
             int imgX = imgPos.at<float>(0, 0) / imgPos.at<float>(2, 0);
             int imgY = imgPos.at<float>(1, 0) / imgPos.at<float>(2, 0);
-
-            if(imgX >= 0 && imgY >= 0 && imgX < height && imgY < width && src[j].at<uchar>(imgX, imgY) != 0){
-            //if(imgX < 0 || imgY < 0 || imgX >= height || imgY >= width || src[j].at<uchar>(imgX, imgY) == 0){
+            //the world coordinate point out of range or is black(background)
+            if(imgX < 0 || imgY < 0 || imgY >= height || imgX >= width || src[j].at<uchar>(imgY, imgX) == 0){
               background = true;
               break;
             }
           }
-          if(!background){
-            //is foreground, write the point into file
-            fprintf(output, "%d %d %d\n", x, y, z);
-
-          }
+          //write foreground point into file
+          if(!background) fprintf(output, "%d %d %d\n", x, y, z);
         }
       }
     }
     fclose(output);
   }
-
   return 0;
 }
