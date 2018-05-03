7be70d82203287fc8c44a508da06fd5b8ec1b845
“welinn” <susutruelove@gmail.com>
2018-05-03 13:13:30 +0800

    fail

--- midterm/midterm.cpp
+++ midterm/midterm.cpp
@@ -7,7 +7,7 @@ Mat src1, src2;
 vector<Point2f> pointL, pointR;
 vector<Point3f> normalL, normalR;
 Mat T, TP, F, epipoleL, epipoleR, Te_L, Te_R;
-Mat R_L, R_R, RTe_L, RTe_R, H_L, H_R;
+Mat R_L, R_R, RTe_L, RTe_R, H_L, H_R, Ha;
 float error_F;
 
 void setPoints();
@@ -20,9 +20,6 @@ void calR();
 void calHa();
 
 int main(int argc, char* argv[]){
-
-  Mat dst1, dst2;
-  int width, height;
 
   src1 = imread("./L.JPG");
   if(!src1.data){
@@ -38,14 +35,16 @@ int main(int argc, char* argv[]){
   error_F = 1e-3;
   setPoints();
   calT();
+cout << T << endl;
   normalizePoint();
   calF();
+cout << F << endl;
   while(!calError()){
     calF();
   }
   calF(1); //ch6-1, P.57, t = 0
   calEpipole();
-
+cout << F << endl;
   //cal T
   Mat trans = (Mat_<float>(3, 3) <<
       1, 0, -src1.cols,
@@ -56,7 +55,8 @@ int main(int argc, char* argv[]){
   Te_R = trans * epipoleR;
 
   calR();
-
+cout << R_L << endl;
+cout << R_R << endl;
   //cal G
   Mat GL = (Mat_<float>(3, 3) <<
       1, 0, 0,
@@ -71,11 +71,17 @@ int main(int argc, char* argv[]){
   H_L = GL * R_L * trans;
   H_R = GR * R_R * trans;
 
-  //TODO
   calHa();
-  //imwrite("output.jpg", dst);
-  //imshow("test", dst);
-  //waitKey();
+
+  trans.at<float>(0, 2) *= -1;
+  trans.at<float>(1, 2) *= -1;
+
+  Mat dst(src1.rows, src1.cols, CV_8UC3);
+  warpPerspective(src1, dst, trans * Ha * H_L, dst.size());
+  imwrite("outputL.jpg", dst);
+
+  warpPerspective(src2, dst, trans * H_R, dst.size());
+  imwrite("outputR.jpg", dst);
 
   return 0;
 }
@@ -96,17 +102,27 @@ void setPoints(){
 }
 
 void calT(){
-  float xAvg, yAvg, length;
+  float length;
   Mat tmp1, tmp2;
+  Point2f avgL(0, 0), avgR(0, 0), tmp;//L, tmpR;
+  int i;
+
+  for(i = 0; i < pointL.size(); i++){
+    avgL.x += pointL[i].x;
+    avgL.y += pointL[i].y;
+    avgR.x += pointR[i].x;
+    avgR.y += pointR[i].y;
+  }
+  avgL.x /= pointL.size();
+  avgL.y /= pointL.size();
+  avgR.x /= pointR.size();
+  avgR.y /= pointR.size();
 
   //L img
-  for(int i = 0, xAvg = 0, yAvg = 0, length = 0; i < pointL.size(); i++){
-    xAvg += pointL[i].x;
-    yAvg += pointL[i].y;
-    length += sqrt(pointL[i].x * pointL[i].x + pointL[i].y * pointL[i].y);
+  for(i = 0, length = 0; i < pointL.size(); i++){
+    tmp = pointL[i] - avgL;
+    length += norm(tmp);
   }
-  xAvg /= pointL.size();
-  yAvg /= pointL.size();
   length /= pointL.size();
 
   tmp1 = (Mat_<float>(3, 3) <<
@@ -114,20 +130,17 @@ void calT(){
       0, sqrt(2) / length, 0,
       0,                0, 1);
   tmp2 = (Mat_<float>(3, 3) <<
-      1, 0, -xAvg,
-      0, 1, -yAvg,
+      1, 0, -avgL.x,
+      0, 1, -avgL.y,
       0, 0,     1);
 
   T = tmp1 * tmp2;
 
   //R img
-  for(int i = 0, xAvg = 0, yAvg = 0, length = 0; i < pointR.size(); i++){
-    xAvg += pointR[i].x;
-    yAvg += pointR[i].y;
-    length += sqrt(pointR[i].x * pointR[i].x + pointR[i].y * pointR[i].y);
+  for(int i = 0, length = 0; i < pointR.size(); i++){
+    tmp = pointR[i] - avgR;
+    length += norm(tmp);
   }
-  xAvg /= pointR.size();
-  yAvg /= pointR.size();
   length /= pointR.size();
 
   tmp1 = (Mat_<float>(3, 3) <<
@@ -135,12 +148,11 @@ void calT(){
       0, sqrt(2) / length, 0,
       0,                0, 1);
   tmp2 = (Mat_<float>(3, 3) <<
-      1, 0, -xAvg,
-      0, 1, -yAvg,
+      1, 0, -avgR.x,
+      0, 1, -avgR.y,
       0, 0,     1);
 
   TP = tmp1 * tmp2;
-
 }
 
 void normalizePoint(){
@@ -180,9 +192,13 @@ void calF(int mode){
     F = TP.t() * Fh * T;
   else{
     SVD::compute(Fh, w, u, vt, SVD::FULL_UV);
-    w.at<float>(2, 2) = 0;
-    F = TP.t() * (u * w * vt) * T;
+    Mat s = (Mat_<float>(3, 3) <<
+        w.at<float>(0, 0), 0, 0,
+        0, w.at<float>(1, 0), 0,
+        0,                 0, 0);
+    F = TP.t() * (u * s * vt) * T;
   }
+
 }
 
 bool calError(){
@@ -203,28 +219,24 @@ bool calError(){
   if(max > error_F){
     normalL[index] = normalL[normalL.size()];
     normalR[index] = normalR[normalR.size()];
-    //pointL[index] = pointL[pointL.size()];
-    //pointR[index] = pointR[pointR.size()];
     normalL.pop_back();
     normalR.pop_back();
-    //pointL.pop_back();
-    //pointR.pop_back();
+
+    pointL[index] = pointL[pointL.size()];
+    pointR[index] = pointR[pointR.size()];
+    pointL.pop_back();
+    pointR.pop_back();
     return false;
   }
   return true;
 }
 
 void calEpipole(){
-  Mat l1 = (Mat_<float>(3, 1) << normalL[0].x, normalL[0].y, normalL[0].z);
-  Mat l2 = (Mat_<float>(3, 1) << normalL[1].x, normalL[1].y, normalL[1].z);
-  Mat r1 = (Mat_<float>(3, 1) << normalR[0].x, normalR[0].y, normalR[0].z);
-  Mat r2 = (Mat_<float>(3, 1) << normalR[1].x, normalR[1].y, normalR[1].z);
-/*
-  Mat l1 = (Mat_<float>(3, 1) << pointL[0].x, pointL[0].y, pointL[0].z);
-  Mat l2 = (Mat_<float>(3, 1) << pointL[1].x, pointL[1].y, pointL[1].z);
-  Mat r1 = (Mat_<float>(3, 1) << pointR[0].x, pointR[0].y, pointR[0].z);
-  Mat r2 = (Mat_<float>(3, 1) << pointR[1].x, pointR[1].y, pointR[1].z);
-*/
+  Mat l1 = (Mat_<float>(3, 1) << pointL[0].x, pointL[0].y, 1);
+  Mat l2 = (Mat_<float>(3, 1) << pointL[1].x, pointL[1].y, 1);
+  Mat r1 = (Mat_<float>(3, 1) << pointR[0].x, pointR[0].y, 1);
+  Mat r2 = (Mat_<float>(3, 1) << pointR[1].x, pointR[1].y, 1);
+
   epipoleL = (F.t() * r1).cross(F.t() * r2);
   epipoleR = (F     * l1).cross(F     * l2);
   epipoleL /= epipoleL.at<float>(2, 0);
@@ -246,4 +258,44 @@ void calR(){
       sin(angleR),  cos(angleR), 0,
                 0,            0, 1);
   RTe_R = R_R * Te_R;
+}
+
+void calHa(){
+  Mat HaTmp1(5, 5, CV_32F, Scalar(0));
+  Mat HaTmp2(5, 1, CV_32F, Scalar(0));
+  for(int i = 0; i < pointL.size(); i++){
+    Mat x  = (Mat_<float>(3, 1) << pointL[i].x, pointL[i].y, 1);
+    Mat xp = (Mat_<float>(3, 1) << pointR[i].x, pointR[i].y, 1);
+    x  = H_L * x;
+    xp = H_R * xp;
+    x.at<float>(0, 0) /= x.at<float>(2,0);
+    x.at<float>(1, 0) /= x.at<float>(2,0);
+    xp.at<float>(0, 0) /= xp.at<float>(2,0);
+    xp.at<float>(1, 0) /= xp.at<float>(2,0);
+    HaTmp1.at<float>(0, 0) += x.at<float>(0, 0) * x.at<float>(0, 0);
+    HaTmp1.at<float>(0, 1) += x.at<float>(0, 0) * x.at<float>(1, 0);
+    HaTmp1.at<float>(0, 2) += x.at<float>(0, 0);
+    HaTmp1.at<float>(1, 0) += x.at<float>(0, 0) * x.at<float>(1, 0);
+    HaTmp1.at<float>(1, 1) += x.at<float>(1, 0) * x.at<float>(1, 0);
+    HaTmp1.at<float>(1, 2) += x.at<float>(1, 0);
+    HaTmp1.at<float>(2, 0) += x.at<float>(0, 0);
+    HaTmp1.at<float>(2, 1) += x.at<float>(1, 0);
+    HaTmp1.at<float>(3, 3) += x.at<float>(1, 0) * x.at<float>(1, 0);
+    HaTmp1.at<float>(3, 4) += x.at<float>(1, 0);
+    HaTmp1.at<float>(4, 3) += x.at<float>(1, 0);
+
+    HaTmp2.at<float>(0, 0) +=  x.at<float>(0, 0) * xp.at<float>(0, 0);
+    HaTmp2.at<float>(1, 0) +=  x.at<float>(1, 0) * xp.at<float>(0, 0);
+    HaTmp2.at<float>(2, 0) += xp.at<float>(0, 0);
+    HaTmp2.at<float>(3, 0) +=  x.at<float>(1, 0) * xp.at<float>(1, 0);
+    HaTmp2.at<float>(4, 0) += xp.at<float>(1, 0);
+
+  }
+  HaTmp1.at<float>(2, 2) = HaTmp1.at<float>(4, 4) = pointL.size();
+  Mat HaTmp3 = HaTmp1.inv() * HaTmp2;
+  Ha = (Mat_<float>(3, 3) <<
+    HaTmp3.at<float>(0, 0), HaTmp3.at<float>(1, 0), HaTmp3.at<float>(2, 0),
+                         0, HaTmp3.at<float>(3, 0), HaTmp3.at<float>(4, 0),
+                         0,                      0,                     1);
+
 }
