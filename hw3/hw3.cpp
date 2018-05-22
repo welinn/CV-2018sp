e58ff60fa7c4486d7cf2f13c17464ae212dea2ed
“welinn” <susutruelove@gmail.com>
2018-05-22 13:46:05 +0800

    finish

--- hw3/hw3.cpp
+++ hw3/hw3.cpp
@@ -7,6 +7,8 @@ void setSquare(Mat*, Mat*, Mat*, Mat*);
 void setA(Mat*, Mat, Mat, Mat);
 void getInvOmega(Mat*, Mat);
 void calK(Mat*, Mat);
+Mat calRtMatrix(Mat, Mat, Mat);
+double calLen(double, double, double);
 
 int main(int argc, char *argv[]){
 
@@ -36,19 +38,63 @@ int main(int argc, char *argv[]){
   Mat rb = k.inv() * hb;
   Mat rc = k.inv() * hc;
 
+  Mat ha3 = ha.colRange(2, 3);
+  Mat hb3 = hb.colRange(2, 3);
+  Mat hc3 = hc.colRange(2, 3);
 
+  Mat RtA = calRtMatrix(ra, ha3, k);
+  Mat RtB = calRtMatrix(rb, hb3, k);
+  Mat RtC = calRtMatrix(rc, hc3, k);
+
+  Mat homoRow4 = (Mat_<double>(1, 4) << 0, 0, 0, 1);
+  Mat localA = RtA.clone();
+  Mat localB = RtB.clone();
+  Mat localC = RtC.clone();
+  localA.push_back(homoRow4);
+  localB.push_back(homoRow4);
+  localC.push_back(homoRow4);
+
+  localA = localA.inv();
+  localB = localB.inv();
+  localC = localC.inv();
+
+  //get camera local position
+  Mat localPosA = localA.colRange(3, 4).clone();
+  Mat localPosB = localB.colRange(3, 4).clone();
+  Mat localPosC = localC.colRange(3, 4).clone();
+
+  Mat rotA = (Mat_<double>(4, 4) <<
+      0, 0, 1, 0,
+      1, 0, 0, 0,
+      0, 1, 0, 0,
+      0, 0, 0, 1);
+  Mat rotB = (Mat_<double>(4, 4) <<
+      0, 1, 0, 0,
+      0, 0, 1, 0,
+      1, 0, 0, 0,
+      0, 0, 0, 1);
+  Mat rotC = (Mat_<double>(4, 4) <<
+      1, 0, 0, 0,
+      0, 1, 0, 0,
+      0, 0, 1, 50,
+      0, 0, 0, 1);
+
+  Mat worldPosA = rotA * localPosA;
+  Mat worldPosB = rotB * localPosB;
+  Mat worldPosC = rotC * localPosC;
+
+  cout << worldPosA << endl;
+  cout << worldPosB << endl;
+  cout << worldPosC << endl;
 
   return 0;
 }
 
 void setSquare(Mat *a, Mat *b, Mat *c, Mat *u){
-
-  *a = (Mat_<double>(4, 2) << );
-  *b = (Mat_<double>(4, 2) << );
-  *c = (Mat_<double>(4, 2) << );
-  //左上,左下,右下,右上
-  *u = (Mat_<double>(4, 2) << 0, 0,  0, 50,  50, 50,  50, 0);
-
+  *a = (Mat_<double>(4, 2) << 436, 571, 449, 229, 163, 155, 209, 429);
+  *b = (Mat_<double>(4, 2) << 436, 571, 584, 400, 625, 141, 449, 229);
+  *c = (Mat_<double>(4, 2) << 449, 229, 163, 155, 381, 106, 625, 141);
+  *u = (Mat_<double>(4, 2) << 0, 0, 0, 50, 50, 50, 50, 0);
 }
 
 void setA(Mat *A, Mat a, Mat b, Mat c){
@@ -62,7 +108,6 @@ void setA(Mat *A, Mat a, Mat b, Mat c){
   b10 = b.at<double>(1, 0); b11 = b.at<double>(1, 1); b12 = b.at<double>(1, 2);
   c00 = c.at<double>(0, 0); c01 = c.at<double>(0, 1); c02 = c.at<double>(0, 2);
   c10 = c.at<double>(1, 0); c11 = c.at<double>(1, 1); c12 = c.at<double>(1, 2);
-
 
   *A = (Mat_<double>(6, 6) <<
       a00*a10, a00*a11 + a01*a10, a00*a12 + a02*a10, a01*a11, a01*a12 + a02*a11, a02*a12,
@@ -98,4 +143,33 @@ void calK(Mat* k, Mat inw){
       a, b, c,
       0, d, e,
       0, 0, 1);
+}
+
+double calLen(double a, double b, double c){
+  return sqrt(a*a + b*b + c*c);
+}
+
+Mat calRtMatrix(Mat r, Mat h3, Mat k){
+  double r1Len = calLen(r.at<double>(0, 0), r.at<double>(1, 0), r.at<double>(2, 0));
+  double r2Len = calLen(r.at<double>(0, 1), r.at<double>(1, 1), r.at<double>(2, 1));
+
+  Mat r1 = r.colRange(0, 1).clone();
+  r1 /= r1Len;
+
+  Mat r2 = r.colRange(1, 2).clone();
+  r2 /= r2Len;
+
+  Mat r3 = r1.cross(r2);
+  //normalize
+  double r3Len = calLen(r3.at<double>(0, 0), r3.at<double>(1, 0), r3.at<double>(2, 0));
+  r3 /= r3Len;
+  r2 = r3.cross(r1);
+
+  Mat t = k.inv() * h3 / r1Len;
+  Mat rt = (Mat_<double>(3, 4) <<
+      r1.at<double>(0, 0), r2.at<double>(0, 0), r3.at<double>(0, 0), t.at<double>(0, 0),
+      r1.at<double>(1, 0), r2.at<double>(1, 0), r3.at<double>(1, 0), t.at<double>(1, 0),
+      r1.at<double>(2, 0), r2.at<double>(2, 0), r3.at<double>(2, 0), t.at<double>(2, 0));
+
+  return rt;
 }
